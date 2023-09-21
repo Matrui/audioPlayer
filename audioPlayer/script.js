3fe89fb32600e426affaa4767c3511ebd47f04b4
Matrui <yanmatr@gmail.com>
2023-09-21 12:04:44 +0200

    feat: play/pause add (21.09.2023 12:03 PM)

--- audioPlayer/script.js
+++ audioPlayer/script.js
@@ -1,4 +1,36 @@
-let screenWidth = window.innerWidth;
-let screenHeight = window.innerHeight;
+let audio = document.querySelectorAll('.audio');
 
-console.log(screenHeight);
+let playBtn = document.querySelector('.play');
+let pauseBtn = document.querySelector('.pause');
+let leftBtn = document.querySelector('.left');
+let rightBtn = document.querySelector('.right');
+
+
+console.log(audio);
+
+audio.forEach(function(element){
+    playBtn.addEventListener("click", () => {
+        if(element.classList.contains('plaing')){
+            play(element);
+        };
+    });
+    pauseBtn.addEventListener("click", () => {
+        if(element.classList.contains('plaing')){
+            play(element);
+        };
+    });
+})
+
+
+
+function play(element){
+    if(element.paused){
+        element.play();
+        playBtn.style.display = "none";
+        pauseBtn.style.display = "inline-block";
+    } else{
+        element.pause();
+        playBtn.style.display = "inline-block";
+        pauseBtn.style.display = "none";
+    }
+};
