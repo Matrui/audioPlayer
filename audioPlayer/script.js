0a18452630a6ed6e9b0e5830eda0f7f72154060f
Matrui <yanmatr@gmail.com>
2023-09-24 23:11:46 +0200

    feat: add progress bar (24.09.2023 23:11)

--- audioPlayer/script.js
+++ audioPlayer/script.js
@@ -15,8 +15,73 @@ let songName0 = ["Don't Hurt Yourself", "Beyonce"];
 let songName1 = ["Don't Start Now", "Dua Lipa"];
 let songs = [songName0, songName1];
 
+let currentTiming = document.querySelector('.current');
+let songLength = document.querySelector('.song-length');
+
 
 body.style.backgroundImage = 'url("media/images/c0.png")';
+
+function timeToSec(song){
+    let min = Math.floor(song.duration / 60);
+    let sec = Math.floor(song.duration % 60);
+    if(sec<10){
+        sec = '0'+ Math.floor(song.duration % 60);
+    }
+    return `${min}:${sec}`;
+};
+
+function currentTimeToSec(song){
+    let min = Math.floor(song.currentTime / 60);
+    let sec = Math.floor(song.currentTime % 60);
+    if(sec<10){
+        sec = '0'+ Math.floor(song.currentTime % 60);
+    }
+    return `${min}:${sec}`;
+};
+
+function currentDuration(songNumber){
+
+    audio[songNumber].addEventListener( "loadeddata", () => {
+        songLength.textContent = timeToSec(audio[songNumber]);
+        currentTiming.textContent = currentTimeToSec(audio[songNumber]);
+        },
+    );
+
+    songLength.textContent = timeToSec(audio[songNumber]);
+
+    audio[songNumber].addEventListener( "timeupdate", () => {
+        currentTiming.textContent = currentTimeToSec(audio[songNumber]);
+        let width = (audio[songNumber].currentTime / audio[songNumber].duration) * 100;
+        document.querySelector('.progress').style.width = `${width}%`;
+        console.log(width);
+    });
+
+    artist.innerHTML = songs[(songNumber)][1];
+    song.innerHTML = songs[(songNumber)][0];
+    body.style.backgroundImage = `url("media/images/c${songNumber}.png")`;
+    songImg.src = `media/images/c${songNumber}.png`;
+    audio[songNumber].classList.add('plaing')
+    audio[songNumber].currentTime = 0;
+
+};
+
+currentDuration(0);
+
+function timeLine(time){
+    for(let i = 0; i < audio.length; i++){
+    if(audio[i].classList.contains('plaing')){
+        let currentWidth = this.clientWidth;
+        let userClick = time.offsetX;
+        const duration = audio[i].duration;
+        audio[i].currentTime = (userClick/currentWidth) * duration;
+        if(audio[i].paused){
+            play(audio[i]);
+        }
+    };
+    };
+}
+
+document.querySelector('.song-line').addEventListener("click", timeLine);
 
 function play(element){
     if(element.paused){
@@ -50,21 +115,11 @@ rightBtn.addEventListener("click", () => {
             }
             audio[i].classList.remove('plaing')
             if(i === (audio.length - 1)){
-                artist.innerHTML = songName0[1];
-                song.innerHTML = songName0[0];
-                body.style.backgroundImage = `url("media/images/c${0}.png")`;
-                songImg.src = `media/images/c${0}.png`;
-                audio[0].classList.add('plaing')
-                audio[0].currentTime = 0;
+                currentDuration(0);
                 play(audio[0]);
                 return;
             }else{
-                artist.innerHTML = songs[(i+1)][1];
-                song.innerHTML = songs[(i+1)][0];
-                body.style.backgroundImage = `url("media/images/c${i+1}.png")`;
-                songImg.src = `media/images/c${i+1}.png`;
-                audio[i+1].classList.add('plaing')
-                audio[i+1].currentTime = 0;
+                currentDuration(i+1);
                 play(audio[i+1]);
                 return;
             }
@@ -81,21 +136,11 @@ leftBtn.addEventListener("click", () => {
             }
             audio[i].classList.remove('plaing')
             if(i === 0){
-                artist.innerHTML = songs[(audio.length - 1)][1];
-                song.innerHTML = songs[(audio.length - 1)][0];
-                body.style.backgroundImage = `url("media/images/c${(audio.length - 1)}.png")`;
-                songImg.src = `media/images/c${(audio.length - 1)}.png`;
-                audio[(audio.length - 1)].classList.add('plaing')
-                audio[(audio.length - 1)].currentTime = 0;
+                currentDuration((audio.length - 1));
                 play(audio[(audio.length - 1)]);
                 return;
             }else{
-                artist.innerHTML = songs[(i - 1)][1];
-                song.innerHTML = songs[(i - 1)][0];
-                body.style.backgroundImage = `url("media/images/c${(i - 1)}.png")`;
-                songImg.src = `media/images/c${(i - 1)}.png`;
-                audio[i-1].classList.add('plaing')
-                audio[i - 1].currentTime = 0;
+                currentDuration(i - 1);
                 play(audio[i-1]);
                 return;
             }
